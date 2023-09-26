34f5ddcd9299dcd292d75b9770e9258b030fc65c
a943356255 <943356255@qq.com>
2023-09-26 15:36:44 +0800

    feat: 新增具体的测试实例

---
+++ src/main/java/service/impl/AServiceImpl.java
@@ -0,0 +1,11 @@
+package service.impl;
+
+import service.AService;
+
+public class AServiceImpl implements AService {
+
+    @Override
+    public void sayHello() {
+        System.out.println("test say hello");
+    }
+}
