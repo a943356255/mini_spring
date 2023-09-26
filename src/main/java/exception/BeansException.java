c5611fbb964383bdfe28880463d0e0965cf1287a
a943356255 <943356255@qq.com>
2023-09-26 15:36:58 +0800

    feat: 新增具体异常类

---
+++ src/main/java/exception/BeansException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class BeansException extends Exception {
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+    public BeansException() {
+
+    }
+}
