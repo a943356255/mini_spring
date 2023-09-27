50c58e425442ba1151073208377d118d99bec98c
a943356255 <943356255@qq.com>
2023-09-27 12:30:11 +0800

    fix: 修改BeanDefinition的定义，新增属性

--- src/main/java/entity/BeanDefinition.java
+++ src/main/java/entity/BeanDefinition.java
@@ -2,12 +2,90 @@ package entity;
 
 public class BeanDefinition {
 
+    String SCOPE_SINGLETON = "singleton";
+    String SCOPE_PROTOTYPE = "prototype";
+
+    // 要不要在加载的时候初始化
+    private boolean lazyInit = false;
+    // 记录属性之间的依赖关系
+    private String[] dependsOn;
+    private ArgumentValues constructorArgumentValues;
+    private PropertyValues propertyValues;
+    // 初始化方法的名称
+    private String initMethodName;
+    private volatile Object beanClass;
     private String id;
     private String className;
+    // 标识是单例还是原型模式
+    private String scope = SCOPE_SINGLETON;
 
     public BeanDefinition(String id, String className) {
         this.id = id;
         this.className = className;
+    }
+
+    public String getSCOPE_SINGLETON() {
+        return SCOPE_SINGLETON;
+    }
+
+    public void setSCOPE_SINGLETON(String SCOPE_SINGLETON) {
+        this.SCOPE_SINGLETON = SCOPE_SINGLETON;
+    }
+
+    public String getSCOPE_PROTOTYPE() {
+        return SCOPE_PROTOTYPE;
+    }
+
+    public void setSCOPE_PROTOTYPE(String SCOPE_PROTOTYPE) {
+        this.SCOPE_PROTOTYPE = SCOPE_PROTOTYPE;
+    }
+
+    public boolean isLazyInit() {
+        return lazyInit;
+    }
+
+    public void setLazyInit(boolean lazyInit) {
+        this.lazyInit = lazyInit;
+    }
+
+    public String[] getDependsOn() {
+        return dependsOn;
+    }
+
+    public void setDependsOn(String[] dependsOn) {
+        this.dependsOn = dependsOn;
+    }
+
+    public ArgumentValues getConstructorArgumentValues() {
+        return constructorArgumentValues;
+    }
+
+    public void setConstructorArgumentValues(ArgumentValues constructorArgumentValues) {
+        this.constructorArgumentValues = constructorArgumentValues;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
+    }
+
+    public String getInitMethodName() {
+        return initMethodName;
+    }
+
+    public void setInitMethodName(String initMethodName) {
+        this.initMethodName = initMethodName;
+    }
+
+    public Object getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Object beanClass) {
+        this.beanClass = beanClass;
     }
 
     public String getId() {
@@ -24,5 +102,13 @@ public class BeanDefinition {
 
     public void setClassName(String className) {
         this.className = className;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
     }
 }
