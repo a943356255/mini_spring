a614ac8a2b8d5a88c686accebe0afeebf3096a33
a943356255 <943356255@qq.com>
2023-09-28 09:51:00 +0800

    feat: 新增测试接口

---
+++ src/main/java/service/BaseBaseService.java
@@ -0,0 +1,16 @@
+package service;
+
+import service.impl.AServiceImpl;
+
+public class BaseBaseService {
+
+    private AServiceImpl as;
+
+    public AServiceImpl getAs() {
+        return as;
+    }
+
+    public void setAs(AServiceImpl as) {
+        this.as = as;
+    }
+}
