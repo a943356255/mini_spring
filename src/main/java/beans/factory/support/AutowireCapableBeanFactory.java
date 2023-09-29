f9931068c5641f4a76f0fe39867334dc6a34278a
a943356255 <943356255@qq.com>
2023-09-29 20:19:26 +0800

    feat: 新增工厂实现

---
+++ src/main/java/beans/factory/support/AutowireCapableBeanFactory.java
@@ -0,0 +1,50 @@
+package beans.factory.support;
+
+import exception.BeansException;
+import utils.AutowiredAnnotationBeanPostProcessor;
+import utils.BeanPostProcessor;
+
+import java.util.*;
+
+public class AutowireCapableBeanFactory extends AbstractBeanFactory {
+
+    private final List<AutowiredAnnotationBeanPostProcessor> beanPostProcessors = new ArrayList<>();
+
+    public void addBeanPostProcessor(AutowiredAnnotationBeanPostProcessor beanPostProcessor) {
+        this.beanPostProcessors.remove(beanPostProcessor);
+        this.beanPostProcessors.add(beanPostProcessor);
+    }
+
+    public int getBeanPostProcessorCount() {
+        return this.beanPostProcessors.size();
+    }
+
+    public List<AutowiredAnnotationBeanPostProcessor> getBeanPostProcessors() {
+        return this.beanPostProcessors;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorBeforeInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (AutowiredAnnotationBeanPostProcessor beanProcessor : getBeanPostProcessors()) {
+            beanProcessor.setBeanFactory(this);
+            result = beanProcessor.postProcessBeforeInitialization(result, beanName);
+            if (result == null) {
+                return null;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Object applyBeanPostProcessorAfterInitialization(Object existingBean, String beanName) throws BeansException {
+        Object result = existingBean;
+        for (BeanPostProcessor beanProcessor : getBeanPostProcessors()) {
+            result = beanProcessor.postProcessAfterInitialization(result, beanName);
+            if (result == null) {
+                return null;
+            }
+        }
+        return result;
+    }
+}
