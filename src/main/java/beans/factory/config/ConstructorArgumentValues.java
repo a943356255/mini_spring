bee9bb1b5b87d829b65878aaee08a64883398228
a943356255 <943356255@qq.com>
2023-09-29 20:19:02 +0800

    update: 注释无用代码，修改getArgumentCount和isEmpty方法返回的目标集合

--- src/main/java/beans/factory/config/ConstructorArgumentValues.java
+++ src/main/java/beans/factory/config/ConstructorArgumentValues.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class ConstructorArgumentValues {
 
-    private final Map<Integer, ConstructorArgumentValue> indexedArgumentValues = new HashMap<>(0);
-
-    private final List<ConstructorArgumentValue> genericConstructorArgumentValues = new LinkedList<>();
+//    private final Map<Integer, ConstructorArgumentValue> indexedArgumentValues = new HashMap<>(0);
+//
+//    private final List<ConstructorArgumentValue> genericConstructorArgumentValues = new LinkedList<>();
 
     private final List<ConstructorArgumentValue> constructorArgumentValueList = new ArrayList<>();
 
@@ -18,47 +18,47 @@ public class ConstructorArgumentValues {
         this.constructorArgumentValueList.add(constructorArgumentValue);
     }
 
-    private void addArgumentValue(Integer key, ConstructorArgumentValue newValue) {
-        this.indexedArgumentValues.put(key, newValue);
-    }
+//    private void addArgumentValue(Integer key, ConstructorArgumentValue newValue) {
+//        this.indexedArgumentValues.put(key, newValue);
+//    }
 
-    public boolean hasIndexedArgumentValue(int index) {
-        return this.indexedArgumentValues.containsKey(index);
-    }
+//    public boolean hasIndexedArgumentValue(int index) {
+//        return this.indexedArgumentValues.containsKey(index);
+//    }
 
     public ConstructorArgumentValue getIndexedArgumentValue(int index) {
-        return this.indexedArgumentValues.get(index);
+        return this.constructorArgumentValueList.get(index);
     }
 
-    public void addGenericArgumentValue(Object value, String type) {
-        this.genericConstructorArgumentValues.add(new ConstructorArgumentValue(value, type));
-    }
-
-    private void addGenericArgumentValue(ConstructorArgumentValue newValue) {
-        if (newValue.getName() != null) {
-            // 删除掉和newValue的name一样的值
-            this.genericConstructorArgumentValues.removeIf(currentValue -> newValue.getName().equals(currentValue.getName()));
-        }
-        // 新添加
-        this.genericConstructorArgumentValues.add(newValue);
-    }
-
-    public ConstructorArgumentValue getGenericArgumentValue(String requiredName) {
-        for (ConstructorArgumentValue valueHolder : this.genericConstructorArgumentValues) {
-            // 根据requiredName找到对应的实例
-            if (valueHolder.getName() != null && (!valueHolder.getName().equals(requiredName))) {
-                continue;
-            }
-            return valueHolder;
-        }
-        return null;
-    }
+//    public void addGenericArgumentValue(Object value, String type) {
+//        this.genericConstructorArgumentValues.add(new ConstructorArgumentValue(value, type));
+//    }
+//
+//    private void addGenericArgumentValue(ConstructorArgumentValue newValue) {
+//        if (newValue.getName() != null) {
+//            // 删除掉和newValue的name一样的值
+//            this.genericConstructorArgumentValues.removeIf(currentValue -> newValue.getName().equals(currentValue.getName()));
+//        }
+//        // 新添加
+//        this.genericConstructorArgumentValues.add(newValue);
+//    }
+//
+//    public ConstructorArgumentValue getGenericArgumentValue(String requiredName) {
+//        for (ConstructorArgumentValue valueHolder : this.genericConstructorArgumentValues) {
+//            // 根据requiredName找到对应的实例
+//            if (valueHolder.getName() != null && (!valueHolder.getName().equals(requiredName))) {
+//                continue;
+//            }
+//            return valueHolder;
+//        }
+//        return null;
+//    }
 
     public int getArgumentCount() {
-        return this.genericConstructorArgumentValues.size();
+        return this.constructorArgumentValueList.size();
     }
 
     public boolean isEmpty() {
-        return this.genericConstructorArgumentValues.isEmpty();
+        return this.constructorArgumentValueList.isEmpty();
     }
 }
