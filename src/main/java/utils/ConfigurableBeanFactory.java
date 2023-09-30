4bb7cc6424ba7c5c76353cd5db580caddf48f2b0
a943356255 <943356255@qq.com>
2023-09-30 16:01:59 +0800

    feat: 新增接口，维护 Bean 之间的依赖关系以及支持 Bean 处理器

---
+++ src/main/java/utils/ConfigurableBeanFactory.java
@@ -0,0 +1,22 @@
+package utils;
+
+import beans.factory.BeanFactory;
+import beans.factory.config.SingletonBeanRegistry;
+
+public interface ConfigurableBeanFactory extends BeanFactory, SingletonBeanRegistry {
+
+    String SCOPE_SINGLETON = "singleton";
+
+    String SCOPE_PROTOTYPE = "prototype";
+
+    void addBeanPostProcessor(BeanPostProcessor beanPostProcessor);
+
+    int getBeanPostProcessorCount();
+
+    void registerDependentBean(String beanName, String dependentBeanName);
+
+    String[] getDependentBeans(String beanName);
+
+    String[] getDependenciesForBean(String beanName);
+
+}
