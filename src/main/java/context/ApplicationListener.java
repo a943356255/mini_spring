30247657ef1b13d80bfe5b672945ce861957bc1b
a943356255 <943356255@qq.com>
2023-09-30 20:26:45 +0800

    feat: 新增简单的事件发布者 SimpleApplicationEventPublisher

--- src/main/java/context/ApplicationListener.java
+++ src/main/java/context/ApplicationListener.java
@@ -4,7 +4,7 @@ import java.util.EventListener;
 
 public class ApplicationListener implements EventListener {
 
-    void onApplicationEvent(ApplicationEvent event) {
+    public void onApplicationEvent(ApplicationEvent event) {
         System.out.println(event.toString());
     }
 
