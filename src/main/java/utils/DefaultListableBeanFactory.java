96bf467eee46b7963dd56f016a96fdb764eca287
a943356255 <943356255@qq.com>
2023-09-30 20:02:29 +0800

    feat: 新增IoC引擎

---
+++ src/main/java/utils/DefaultListableBeanFactory.java
@@ -0,0 +1,69 @@
+package utils;
+
+import beans.factory.config.AbstractAutowireCapableBeanFactory;
+import beans.factory.config.BeanDefinition;
+import beans.factory.config.ConfigurableListableBeanFactory;
+import exception.BeansException;
+
+import java.util.*;
+
+public class DefaultListableBeanFactory extends AbstractAutowireCapableBeanFactory implements ConfigurableListableBeanFactory {
+
+    ConfigurableListableBeanFactory parentBeanFactory;
+
+    @Override
+    public int getBeanDefinitionCount() {
+        return this.beanDefinitionMap.size();
+    }
+
+    @Override
+    public String[] getBeanDefinitionNames() {
+        return (String[]) this.beanDefinitionNames.toArray();
+    }
+
+    @Override
+    public String[] getBeanNamesForType(Class<?> type) {
+        List<String> result = new ArrayList<>();
+        for (String beanName : this.beanDefinitionNames) {
+            boolean matchFound;
+            BeanDefinition mbd = this.getBeanDefinition(beanName);
+            Class<?> classToMatch = mbd.getClass();
+            if (type.isAssignableFrom(classToMatch)) {
+                matchFound = true;
+            } else {
+                matchFound = false;
+            }
+            if (matchFound) {
+                result.add(beanName);
+            }
+        }
+        return (String[]) result.toArray();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> Map<String, T> getBeansOfType(Class<T> type) throws BeansException {
+        String[] beanNames = getBeanNamesForType(type);
+        Map<String, T> result = new LinkedHashMap<>(beanNames.length);
+        for (String beanName : beanNames) {
+            Object beanInstance = getBean(beanName);
+            result.put(beanName, (T) beanInstance);
+        }
+        return result;
+    }
+
+    @Override
+    public void registerDependentBean(String beanName, String dependentBeanName) {
+
+    }
+
+    @Override
+    public String[] getDependentBeans(String beanName) {
+        return new String[0];
+    }
+
+    @Override
+    public String[] getDependenciesForBean(String beanName) {
+        return new String[0];
+    }
+}
