5d707b4b84bba7214c6f0be32e7cb7735b29df4e
a943356255 <943356255@qq.com>
2023-10-01 20:00:49 +0800

    feat: 新增AbstractApplicationContext抽象类

---
+++ src/main/java/context/AbstractApplicationContext.java
@@ -0,0 +1,83 @@
+package context;
+
+import beans.factory.config.ConfigurableListableBeanFactory;
+import core.env.Environment;
+import exception.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public abstract class AbstractApplicationContext implements ApplicationContext {
+
+    private Environment environment;
+
+    private final List<BeanFactoryPostProcessor> beanFactoryPostProcessors = new ArrayList<>();
+
+    private long startupDate;
+
+    private final AtomicBoolean active = new AtomicBoolean();
+
+    private final AtomicBoolean closed = new AtomicBoolean();
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public Object getBean(String beanName) throws BeansException {
+        return getBeanFactory().getBean(beanName);
+    }
+
+    public List<BeanFactoryPostProcessor> getBeanFactoryPostProcessors() {
+        return this.beanFactoryPostProcessors;
+    }
+
+    public void refresh() throws BeansException, IllegalStateException {
+        postProcessBeanFactory(getBeanFactory());
+        registerBeanPostProcessors(getBeanFactory());
+        initApplicationEventPublisher();
+        onRefresh();
+        registerListeners();
+        finishRefresh();
+    }
+
+    abstract void registerListeners();
+
+    abstract void initApplicationEventPublisher();
+
+    abstract void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory);
+
+    abstract void registerBeanPostProcessors(ConfigurableListableBeanFactory beanFactory);
+
+    abstract void onRefresh();
+
+    abstract void finishRefresh();
+
+    @Override
+    public String getApplicationName() {
+        return "";
+    }
+
+    @Override
+    public long getStartupDate() {
+        return this.startupDate;
+    }
+
+    @Override
+    public abstract ConfigurableListableBeanFactory getBeanFactory() throws IllegalStateException;
+
+    @Override
+    public void addBeanFactoryPostProcessor(BeanFactoryPostProcessor postProcessor) {
+        this.beanFactoryPostProcessors.add(postProcessor);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public boolean isActive() {
+        return true;
+    }
+
+    // 省略包装beanfactory的方法
+}
